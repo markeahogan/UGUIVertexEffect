a9c7bdffcb782641abb14e9f4d6bd5db75512627
Mark Hogan <markeahogan@gmail.com>
2025-04-18 08:05:37 +0100

    Optimize getting child index of StaggeredChildren
    It was iterating the heirachy per vert before, now just once per transform

--- Runtime/Effects/StaggeredChildrenUIEffect.cs
+++ Runtime/Effects/StaggeredChildrenUIEffect.cs
@@ -14,22 +14,22 @@ namespace PopupAsylum.UIEffects
         public override void ModifyVertex(RectTransform graphicTransform, ref UIVertex vertex)
         {
             if (graphicTransform == transform) { return; }
-            vertex = Stagger(graphicTransform, vertex);
+
+            var childCount = transform.childCount;
+            var childIndex = GetSiblingIndex(graphicTransform);
+            vertex = Stagger(vertex, childCount, childIndex);
         }
 
-        private UIVertex Stagger(RectTransform graphicTransform, UIVertex vertex)
+        private UIVertex Stagger(UIVertex vertex, int childCount, int childIndex)
         {
-            var childCount = transform.childCount;
             var pcof = Mathf.Abs(perChildOffset);
             offset = pcof - (show * childCount * pcof);
 
-            var childIndex = GetSiblingIndex(graphicTransform);
             vertex.position += Mathf.Max(0, (offset + pcof * childIndex)) * (perChildOffset >= 0 ? Vector3.right : Vector3.left);
 
             Color color = vertex.color;
             color.a *= 1 - (childIndex + offset / pcof);
             vertex.color = color;
-
             return vertex;
         }
 
@@ -41,9 +41,12 @@ namespace PopupAsylum.UIEffects
 
         protected override void ModifyVertices(RectTransform graphicTransform, List<UIVertex> verts)
         {
+            var childCount = transform.childCount;
+            var childIndex = GetSiblingIndex(graphicTransform);
+
             for (int i = 0; i < verts.Count; i++)
             {
-                verts[i] = Stagger(graphicTransform, verts[i]);
+                verts[i] = Stagger(verts[i], childCount, childIndex);
             }
         }
     }
